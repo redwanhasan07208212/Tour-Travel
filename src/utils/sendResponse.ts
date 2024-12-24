25077478a7157f7dd1e5bd1b7b97661a5b39272a
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-24 11:03:47 +0600

    change some property

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -1,20 +1,17 @@
-import { Response } from 'express';
+import { Response } from 'express'
 
 type TData<T> = {
-  statusCode : number;
-  success:boolean;
-  message:string;
-  data:T
+  statusCode: number
+  success: boolean
+  message: string
+  data?: T
 }
-const sendResponse = <T>(
-  res: Response,
-  data: TData<T>,
-) => {
+const sendResponse = <T>(res: Response, data: TData<T>) => {
   res.status(data?.statusCode).json({
     success: data.success,
     message: data.message,
     data: data.data,
-  });
-};
+  })
+}
 
-export default sendResponse;
+export default sendResponse
